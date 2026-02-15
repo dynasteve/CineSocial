2b0a29709f61d7c6e41d5ce06556e7bddd03fbf8
Stephen Akande <tosisiyesteve@gmail.com>
2026-02-15 15:44:22 +0100

    Add Auth Test View

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class TestAuthView(APIView):
+  def get(self, req):
+    return Response({"message": "Authenticated successfully"})
