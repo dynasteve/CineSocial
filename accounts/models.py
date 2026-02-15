a7077f8808f2acb640fed2c1bd54955958c8e623
Stephen Akande <tosisiyesteve@gmail.com>
2026-02-15 14:49:54 +0100

    Implement Custom User

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+class CustomUser(AbstractUser):
+  bio = models.TextField(blank=True)
+  avatar = models.ImageField(upload_to="avatars/", null=True, blank=True)
+  is_admin = models.BooleanField(default=False)
 
-# Create your models here.
+  def __str__(self):
+      return self.username
