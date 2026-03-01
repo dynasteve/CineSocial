3d9997f5641935a433014fac0c980d39513f36dc
Stephen Akande <tosisiyesteve@gmail.com>
2026-03-01 06:07:40 +0100

    Implement toggle for favourite and watchlater list

--- movies/urls.py
+++ movies/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
   path("search/", MovieSearchView.as_view(), name="movie-search"),
   path("<int:tmdb_id>/", MovieDetailView.as_view(), name="movie-detail"),
-  path("<int:tmdb_id>/review/", MovieReviewCreateView.as_view(), name="movie-review-create")
+  path("<int:tmdb_id>/review/", MovieReviewCreateView.as_view(), name="movie-review-create"),
+  path("<int:tmdb_id>/list/", UserMovieListToggleView.as_view(), name="movie-list-toggle"),
+  path( "list/<str:list_type>/", UserMovieListView.as_view(), name="movie-list-view"),
 ]
